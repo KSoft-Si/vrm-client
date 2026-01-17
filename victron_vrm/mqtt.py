8be999c6402667b2b3b9015257f8a124bfa40836
andraz <9637751+AndyTempel@users.noreply.github.com>
2026-01-17 22:43:44 +0100

    feat: add update frequency and operation mode parameters to MQTT client

--- victron_vrm/mqtt.py
+++ victron_vrm/mqtt.py
@@ -1,4 +1,4 @@
-from victron_mqtt import Hub as VictronMQTTHub
+from victron_mqtt import Hub as VictronMQTTHub, OperationMode
 
 
 class VRMMQTTClient(VictronMQTTHub):
@@ -12,8 +12,13 @@ class VRMMQTTClient(VictronMQTTHub):
         vrm_id: str,
         port: int = 8883,
         use_ssl: bool = True,
+        update_frequency: int | None = None,
+        operation_mode: OperationMode = OperationMode.FULL,
     ):
         """Initialize VRM MQTT Client."""
+        # Safe update_frequency values between 0 and 300 seconds
+        if isinstance(update_frequency, int):
+            update_frequency = max(0, min(update_frequency, 300))
         super().__init__(
             host=host,
             username=username,
@@ -21,4 +26,6 @@ class VRMMQTTClient(VictronMQTTHub):
             port=port,
             use_ssl=use_ssl,
             installation_id=vrm_id,
+            update_frequency_seconds=update_frequency,
+            operation_mode=operation_mode,
         )
